11438da79b99eb2fe5598819ce4557c952d0e3c4
Mariano <marianomarchionna8@gmail.com>
2021-07-30 17:28:13 +0200

    Creazione array di oggetti

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
-// Creare un array di oggetti di studenti.
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
-var studente = {
-    'nome' : 'Mariano',
-    'cognome' : 'Marchionna',
-    'eta' : 21
-}
+// var studente = {
+//     'nome' : 'Mariano',
+//     'cognome' : 'Marchionna',
+//     'eta' : 21
+// }
 
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
-for (var key in studente){
-    console.log(studente[key]);
-}
+// for (var key in studente){
+//     console.log(studente[key]);
+// }
+
+// Creare un array di oggetti di studenti.
+var studenti = [
+    {
+        'nome' : 'Mariano',
+        'cognome' : 'Marchionna',
+        'eta' : 21
+    },
+    {
+        'nome' : 'Paolo',
+        'cognome' : 'Costantino',
+        'eta' : 24
+    },
+    {
+        'nome' : 'Giovanni',
+        'cognome' : 'Mucciaccia',
+        'eta' : 41
+    },
+    {
+        'nome' : 'Domenico',
+        'cognome' : 'Lella',
+        'eta' : 18
+    }
+]
