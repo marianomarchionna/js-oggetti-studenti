5cc7343b97362648979d2fabfe80979bb0773a62
Mariano <marianomarchionna8@gmail.com>
2021-07-30 17:13:53 +0200

    For-in proprietà oggetto

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 // Creare un array di oggetti di studenti.
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
@@ -10,4 +9,7 @@ var studente = {
     'eta' : 21
 }
 
-console.log(studente);
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+for (var key in studente){
+    console.log(studente[key]);
+}
