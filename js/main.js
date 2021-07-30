4da9fbc7d06ce84f6b2edf2958ebfd780d6a0970
Mariano <marianomarchionna8@gmail.com>
2021-07-30 17:10:45 +0200

    Creazione oggetto primo studente

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+// Creare un array di oggetti di studenti.
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+// Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+var studente = {
+    'nome' : 'Mariano',
+    'cognome' : 'Marchionna',
+    'eta' : 21
+}
+
+console.log(studente);
