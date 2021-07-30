ba218b2b2105de4c3c8055e9e043f3105097b6d7
Mariano <marianomarchionna8@gmail.com>
2021-07-30 17:35:33 +0200

    Stampa nome cognome al vettore di oggetti

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
@@ -36,3 +35,10 @@ var studenti = [
         'eta' : 18
     }
 ]
+
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+for(let i = 0 ; i < studenti.length; i++){
+    for (let key in studenti[i]){
+        console.log('Nome: ' + studenti[i].nome + ' Cognome: ' + studenti[i].cognome);  
+    }
+}
